b372f5675758b24bb3c4335ee0645e4c09c6eef6
Garrett Corbin <jgarrettcorbin@gmail.com>
2023-09-05 22:03:06 -0700

    init

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { exec } = require('child_process');
+
+const app = express();
+const port = 3000;
+
+// Middleware to parse JSON request bodies
+app.use(bodyParser.json());
+
+// POST endpoint to accept JSON data
+app.post('/', (req, res) => {
+  const requestData = req.body;
+
+  const command = 'sh /home/garrett/sms.sh "+18043991576" "helloagain"';
+
+  if (error) {
+    console.error(`Error: ${error.message}`);
+    return;
+  }
+
+  if (stderr) {
+    console.error(`Stderr: ${stderr}`);
+    return;
+  }
+
+  console.log(`Command output:\n${stdout}`);
+});
+  res.json({ message: 'Received JSON data:', data: requestData });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
